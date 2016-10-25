707b78df8318cbc6d41553cce5c2a2199bb8209c
Dylan Dunne <dylanadunne@gmail.com>
2016-10-24 23:26:33 -0400

    Fixes #2

---
+++ src/server/api/comments/commentsController.js
@@ -0,0 +1,60 @@
+var Post = require('../comments/commentModel');
+var _ = require('lodash');
+var logger = require('../../util/logger');
+var Comment = require('./commentModel');
+
+
+
+  exports.get = function(req, res) {
+    Comment.find(function(err, comments) {
+      if (!err) {
+        res.json(comments);
+      } else {
+        console.log(err);
+      }
+    });
+  };
+
+  exports.post = function(req, res) {
+    var comment = new Comment({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    comment.save(function(err) {
+      if (!err) {
+        console.log('Created!');
+      } else {
+        console.log(err);
+      }
+    });
+    res.json(comment);
+  };
+
+  exports.getOne = function(req, res) {
+      Comment.findById(req.params.id, function(err, comment) {
+        if (!err) {
+          res.json(comment);
+        } else {
+          console.log(err);
+        }
+      })
+    };
+
+    exports.put = function(req, res) {
+      Comment.findById(req.params.id, function(err, comment) {
+        if (err) {
+          console.log(err);
+        }
+
+        comment.name = req.body.name;
+        comment.description = req.body.description;
+        comment.save(function(err) {
+          if (!err) {
+            console.log('Updated!');
+          } else {
+            console.log(err);
+          }
+        });
+        res.json(comment);
+      })
+    };
